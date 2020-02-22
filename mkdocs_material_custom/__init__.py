943fbebcae0c9d5c920570b966d6b75f57927bdc
facelessuser <faceless.shop@gmail.com>
2020-02-21 22:15:09 -0700

    Rework approach
    Add our theme to the theme list

--- mkdocs_material_custom/__init__.py
+++ mkdocs_material_custom/__init__.py
@@ -5,7 +5,7 @@ import os
 import mkdocs.plugins
 import mkdocs.structure.files
 
-RESOURCE_PATH = os.path.abspath(os.path.dirname(__file__))
+RESOURCE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'theme')
 
 # Nothing to import with all
 __all__ = tuple()
@@ -14,20 +14,20 @@ __all__ = tuple()
 class MaterialCustom(mkdocs.plugins.BasePlugin):
     """Plugin to add custom assets to Material theme."""
 
-    def on_files(self, files, config):
-        """Add additional assets."""
-
-        for item in glob.glob(os.path.join(RESOURCE_PATH, '*.svg').replace('\\', '/'), recursive=True):
-            files.append(item)
-        return mkdocs.structure.files.Files(files)
-
     def on_config(self, config, **kwargs):
         """Add additional assets."""
 
+        config['theme'].dirs.append(RESOURCE_PATH)
+
         config['extra_css'].extend(
-            glob.glob(os.path.join(RESOURCE_PATH, '*.css').replace('\\', '/'), recursive=True)
+            [
+                os.path.basename(f) for f in glob.glob(os.path.join(RESOURCE_PATH, '*.css').replace('\\', '/'))
+            ]
         )
         config['extra_javascript'].extend(
-            glob.glob(os.path.join(RESOURCE_PATH, '*.js').replace('\\', '/'), recursive=True)
+            [
+                os.path.basename(f) for f in glob.glob(os.path.join(RESOURCE_PATH, '*.js').replace('\\', '/'))
+            ]
         )
+
         return config
